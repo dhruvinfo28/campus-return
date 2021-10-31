1b23491761e0c71e85624ce6ce32741e712f42eb
dhruvinfo28 <dhruv.info28@gmail.com>
2021-11-01 01:29:19 +0530

    Second Dose upload and view

---
+++ models/SecondDose.js
@@ -0,0 +1,31 @@
+const db = require('../middlewares/dbConnection');
+
+class SecondDose{
+    constructor(Sdose_id,FD_Document,BId){
+        this.Sdose_id = Sdose_id;
+        this.FD_Document = FD_Document;
+        this.BId = BId;
+    }
+
+    /**
+     * Saves a SecondDose certificate of a particular student
+     * @param rollNumber : Roll number of the student whose certificate we are uploading
+     * @returns  a promise which after resolve gives the response from the database
+     */
+    async save(rollNumber){
+        let saveQuery = "insert into Second_Dose(Sdose_id,FD_Document,student_roll_number,Benificiary_Id) values(?,?,?,?);"
+        return db.promise().query(saveQuery,[this.Sdose_id,this.FD_Document,rollNumber,this.BId]);
+    }
+
+    /**
+     * Fetches all secondDoseCertificates of given student
+     * @param {*} rollNumber 
+     * @returns promise which after resolve gives [rows,fields]
+     */
+    static async getSecondDoseCertificates(rollNumber){
+        let findQuery = "select * from Second_Dose where student_roll_number = ?";
+        return db.promise().query(findQuery,[rollNumber]);
+    } 
+}
+
+module.exports = SecondDose;
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -5,6 +5,7 @@ const Student = require('../models/Student');
 const checkLoginStatus = require('../utilityFunctions/checkLoginStatus')
 const upload = require('../middlewares/multerConfig');
 const FirstDose = require('../models/FirstDose');
+const SecondDose = require('../models/SecondDose');
 
 const router = express.Router();
 
@@ -36,6 +37,7 @@ router.post('/uploadFirstDose',upload.single('fDoseCertificate'),async (req,res)
     try{
         let dbResponse = await fd.save(req.session.student.rollNumber);
         console.log(dbResponse);
+        req.flash('error','Upload complete');
         res.redirect('/dashboard');
     }catch(uploadError){
         console.log("Error in saving to First_Dose: ",uploadError);
@@ -73,4 +75,63 @@ router.get('/viewFirstDose',async (req,res)=>{
         res.end();
     }
 })
+
+/**
+ * To handle post request for second dose certificate upload
+ */
+router.post('/uploadSecondDose',upload.single('sDoseCertificate'),async (req,res)=>{
+    console.log("Reached /dashboard/uploadSecondDose post");
+    let data;
+    try{
+        data = await fs.readFileSync(`${__dirname}/../resources/${req.file.filename}`);
+    }catch(fsError){
+        console.log("Error while reading second dose certificate from server!",fsError);
+        req.flash('error',"Please try again later");
+        res.redirect('/dashboard');
+        return;
+    }
+    
+    let fd = new SecondDose(req.file.filename,data,req.body.secondDoseID);
+    try{
+        let dbResponse = fd.save(req.session.student.rollNumber);   
+        console.log(dbResponse);
+        req.flash('error','Upload complete');
+    }catch(err){
+        console.log("Error while saving second dose certificate!",err);
+        req.flash('error','Please try again later');
+    }
+    res.redirect('/dashboard');
+    fs.unlink(`${__dirname}/../resources/${req.file.filename}`,(err)=>{
+        if(err) console.log("First Dose delete fail: ",err);
+        else console.log(req.file.filename," deleted");
+    });
+    
+})
+
+
+/**
+ * Handles get request to view last firstDose Certificate
+ */
+router.get('/viewSecondDose',async (req,res)=>{
+    let data;
+    try{
+        data = await SecondDose.getSecondDoseCertificates(req.session.student.rollNumber);
+    }catch(err){
+        console.log("Error fetching second dose certificates! ",err);
+        req.flash('error','Try again later');
+        res.redirect('/dashboard');
+        return;
+    }
+    let [rows,fields] = data;
+    if(rows.length==0){
+        req.flash('error','No second dose certificate found');
+        res.redirect('/dashboard');
+        
+    }else{
+        let certi = rows[rows.length-1].FD_Document;
+        res.write(certi);
+        res.end();
+    }
+})
+
 module.exports = router;
