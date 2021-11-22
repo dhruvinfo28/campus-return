bee892cb34c50c05b1a80ba864298eed4283cff2
dhruvinfo28 <dhruv.info28@gmail.com>
2021-11-22 23:27:43 +0530

    Minor deployement fix

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require("express");
 const app = express();
 
 
-app.listen(3000 || process.env.PORT,(err)=>{console.log("App listening at 3000")});
+app.listen(process.env.PORT || 3000,(err)=>{console.log("App listening at 3000")});
 
 module.exports = app;
