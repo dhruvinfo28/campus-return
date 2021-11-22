4fb3f2462f3f29a2864cbb8934fb9985a77e30f6
dhruvinfo28 <dhruv.info28@gmail.com>
2021-11-22 18:28:50 +0530

    Admin dashboard, view application, view-application's view documents

--- models/Application.js
+++ models/Application.js
@@ -1,4 +1,5 @@
 const db = require('../middlewares/dbConnection');
+const FirstDose = require('./FirstDose');
 
 class Application{
     constructor(applicationStatus,applicationReview){
@@ -15,7 +16,91 @@ class Application{
         let findQuery = "select * from Application where student_roll_number = ?";
         return db.promise().query(findQuery,[rollNumber]);
     }
+
+    static async findAll(){
+        let findQuery = "select application.application_id,application.application_status, application.application_review, " + 
+                        "student.student_roll_number,student.student_name,student.student_branch,student.student_year,student.student_programme,student.student_email_id "+
+                        "from application "+
+                        "inner join student "+
+                        "on application.student_roll_number = student.student_roll_number "+
+                        "order by application.application_id;";
+
+        return db.promise().query(findQuery);
+    }
+
+    /**
+     * 
+     * @param {*} applicationId 
+     * @returns Ids of documents associated with the required application_id
+     */
+    static async findCertificateIdsByApplicationId(applicationId){
+        let findQuery1 = "select "+ 
+                        "application.application_id, application.application_status, application.application_review, "+
+                        "fd.Fdose_id "+
+                        "from application "+
+                        "inner join has_certificate_of_fd as fd on application.Application_id = fd.Application_id "+
+                        "where application.application_id = ?;" ;
+        let findQuery2 = "select "+ 
+                        "application.application_id, application.application_status, application.application_review, "+
+                        "sd.Sdose_id "+
+                        "from application "+
+                        "inner join has_certificate_of_sd as sd on application.Application_id = sd.application_id "+
+                        "where application.application_id = ?;" ;
+
+        let findQuery3 = "select "+ 
+                        "application.application_id, application.application_status, application.application_review, "+
+                        "rtpcr.Rtpcr_id "+
+                        "from application "+
+                        "inner join has_report_of as rtpcr on application.Application_id = rtpcr.application_id "+
+                        "where application.application_id = ?;" ;
+        
+        let fdose_id='-1',sdose_id='-1',rtpcr_id='-1';
+
+        //Finding first dose
+        try{
+            let [rows,fields] = await db.promise().query(findQuery1,[applicationId]);
+            if(rows.length != 0){
+                fdose_id = rows[0].Fdose_id;
+            }
+        }catch(err){
+            console.log("Error in finding application and firstDose_id by applicationid: ",applicationId);
+            throw new Error(err);
+        }
+
+        //Finding second dose
+        try{
+            let [rows,fields] = await db.promise().query(findQuery2,[applicationId]);
+            if(rows.length != 0){
+                sdose_id = rows[0].Sdose_id;
+            }
+        }catch(err){
+            console.log("Error in finding application and secondDoseid by applicationid: ",applicationId);
+            throw new Error(err);
+        }
+
+        //Finding third dose
+        try{
+            let [rows,fields] = await db.promise().query(findQuery3,[applicationId]);
+            if(rows.length != 0){
+                rtpcr_id = rows[0].Rtpcr_id;
+            }
+        }catch(err){
+            console.log("Error in finding application and Rtpcr by applicationid: ",applicationId);
+            throw new Error(err);
+        }
+        return [fdose_id,sdose_id,rtpcr_id];
+    }
     
+    static async findById(application_id){
+        let findQuery = "select application.application_id,application.application_status, application.application_review, " + 
+                        "student.student_roll_number,student.student_name,student.student_branch,student.student_year,student.student_programme,student.student_email_id "+
+                        "from application "+
+                        "inner join student "+
+                        "on application.student_roll_number = student.student_roll_number "+
+                        "where application.application_id = ?;";
+
+        return db.promise().query(findQuery,[application_id]);
+    }
 }
 
 module.exports = Application;
--- routes/adminPanelRoutes.js
+++ routes/adminPanelRoutes.js
@@ -1,15 +1,25 @@
 const authenticate = require('../utilityFunctions/checkAdminLoginStatus');
+const Application = require('../models/Application')
+const Student = require('../models/Student')
+const FirstDose = require('../models/FirstDose');
+const SecondDose = require('../models/SecondDose')
+const Rtpcr = require('../models/Rtpcr')
 
-const express = require('express')
+const express = require('express');
 
 const router = express.Router();
 
-
+/**
+ * For handling login get
+ */
 router.get('/',(req,res,next)=>{
     console.log("Reached /admin get");
     res.render('admin-panel/login',{'error':req.flash('error')});
 })
 
+/**
+ * For handling post request of incoming login request
+ */
 router.post('/',(req,res)=>{
     console.log("Reached /admin post");
     const data = req.body;
@@ -24,12 +34,124 @@ router.post('/',(req,res)=>{
     }
 })
 
-router.get('/dashboard',authenticate,(req,res)=>{
+/**
+ * For viewing all applications on the dashboard
+ */
+router.get('/dashboard',authenticate,async (req,res)=>{
     console.log("Reached /admin/dashboard get");
-    res.render('admin-panel/dashboard');
+    let data = [];
+    try{
+        let [rows,fields] = await Application.findAll();
+        data = rows;    
+    }catch(err){
+        console.log("error in finding all applications: ",err);
+        res.redirect('/admin');
+        return;
+    }
+    res.render('admin-panel/dashboard',{data:data,error:req.flash('error')});
 })
 
 
+/**
+ * For particular application request by admin
+ */
+router.get('/application/:id',authenticate,async (req,res)=>{
+    console.log("Reached /admin/application/:id get");
+    let applicationId = req.params.id;
+    if(applicationId === null || applicationId === undefined){
+        req.flash('error','Please select a valid application')
+        res.redirect('/admin/dashboard');
+        return;
+    }
+    let docIds = [], data;
+
+    try{
+        docIds = await Application.findCertificateIdsByApplicationId(applicationId);
+    } catch(err){
+        req.flash('error','Try again later!');
+        res.redirect('/admin/dashboard');
+        console.log("Error in finding certi ids corresponding to application id: ", err);
+        return;
+    }
+    
+    try{
+       let [rows,fields] = await Application.findById(applicationId);
+       data = rows[0];
+
+    }catch(err){
+        req.flash('error','Try again later!');
+        res.redirect('/admin/dashboard');
+        console.log("Error in finding student data: ", err);
+        return;
+    }
+    res.render('admin-panel/view-application',{docIds,data})
+})
+
+/**
+ * For complete view of fdose certificate requested by admin
+ */
+router.get('/application/firstDose/:fdose_id',authenticate,async (req,res)=>{
+    console.log("Reached /admin/application/firstDose/:fdose_id")
+    let fdose_id = req.params.fdose_id;
+    try{
+        let [rows,fields] = await FirstDose.findByID(fdose_id);
+        if(rows.length !=0){
+            res.write(rows[0].FD_Document);
+            res.end();
+        }
+    }
+    catch(err) {
+        req.flash('error','Try again later!');
+        res.redirect('/admin/dashboard');
+        console.log("Error in finding first dose certificate: ", err);
+        return;
+    }
+})
+
+/**
+ * For complete view of sdose certificate requested by admin
+ */
+router.get('/application/secondDose/:sdose_id',authenticate,async (req,res)=>{
+    console.log("Reached /admin/application/secondDose/:sdose_id")
+    let sdose_id = req.params.sdose_id;
+    try{
+        let [rows,fields] = await SecondDose.findByID(sdose_id);
+        if(rows.length !=0){
+            res.write(rows[0].FD_Document);
+            res.end();
+        }
+    }
+    catch(err) {
+        req.flash('error','Try again later!');
+        res.redirect('/admin/dashboard');
+        console.log("Error in finding second dose certificate: ", err);
+        return;
+    }
+})
+
+/**
+ * For complete view of rtpcr certificate requested by admin
+ */
+router.get('/application/rtpcr/:rtpcr_id',authenticate,async (req,res)=>{
+    console.log("Reached /admin/application/rtpcr/:rtpcr_id")
+    let Rtpcr_id = req.params.rtpcr_id;
+    try{
+        let [rows,fields] = await Rtpcr.findByID(Rtpcr_id);
+        if(rows.length !=0){
+            res.write(rows[0].FD_Document);
+            res.end();
+        }
+    }
+    catch(err) {
+        req.flash('error','Try again later!');
+        res.redirect('/admin/dashboard');
+        console.log("Error in finding Rtpcr certificate: ", err);
+        return;
+    }
+})
+/**
+ * For logging the admin out
+ */
 router.post('/logout',(req,res)=>{
     console.log("Reached /admin/logout post");
     if(req.session.admin === 1){
@@ -38,9 +160,5 @@ router.post('/logout',(req,res)=>{
     }
 })
 
-//For testing view application
-router.get('/view',authenticate,(req,res)=>{
-    res.render('admin-panel/view-application');
-})
 
 module.exports = router;
